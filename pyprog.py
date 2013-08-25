298330050551b8b59ae60cef8a4932d3593a6cfb
becky1166 <rebecca.hoath@googlemail.com>
2013-08-25 18:23:50 +0100

    working test

--- pyprog.py
+++ pyprog.py
@@ -3,8 +3,12 @@ from pygame.locals import *
 import pygame.camera
 pygame.init()
 pygame.camera.init()
-cam = pygame.camera.Camera("/dev/video1",(320,240))
+cam = pygame.camera.Camera("/dev/video0",(1920,1080))
+screen = pygame.display.set_mode((1920,1080))
 cam.start()
 image = cam.get_image()
 pygame.image.save(image,'/home/pi/gitDownloads/camera/snap.ICB')
+screen.blit(image,(0,0))
+pygame.display.flip()
 cam.stop()
+nb = raw_input('keyboard')
